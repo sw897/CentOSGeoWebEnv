0e2f20d04db61a3927fbaf65ae192cb9b415540b
sw897 <sunwei.r@gmail.com>
2014-09-07 22:25:32 +0800

    0.1

--- soft/python-env.sh
+++ soft/python-env.sh
@@ -15,15 +15,18 @@ if [ -e "../inc.sh" ]; then
     . "../inc.sh"
 fi
 
-sys_python_packages="/usr/lib64/python${python_version}/site-packages"
+python_version="2.6"
 gdal_version="1.11.0"
 
 pip install virtualenv
 virtualenv --no-site-packages ${python_env_root}
 
+sys_python_packages="/usr/lib64/python${python_version}/site-packages"
+env_python_packages=${python_env_root}/lib/python${python_version}/site-packages/
+
 source ${python_env_root}/bin/activate
 
-install_func "pysqlite"    "${python_site-packages}/pysqlite2"    "./soft/pysqlite.sh"
+install_func "pysqlite"    "${env_python_packages}/pysqlite2"    "./soft/pysqlite.sh"
 pip install simplejson
 pip install lxml
 pip install pillow
@@ -39,9 +42,9 @@ pip install tornado
 
 # gdal
 mv -s ${sys_python_packages}/GDAL-${gdal_version}-py${python_version}-linux-x86_64.egg/EGG-INFO ${sys_python_packages}/GDAL-${gdal_version}-py${python_version}-linux-x86_64.egg/GDAL-${gdal_version}-py${python_version}.egg-info
-ln -s ${sys_python_packages}/GDAL-${gdal_version}-py${python_version}-linux-x86_64.egg/* ${python_site-packages}
+ln -s ${sys_python_packages}/GDAL-${gdal_version}-py${python_version}-linux-x86_64.egg/* ${env_python_packages}
 
 # mapnik
-ln -s ${sys_python_packages}/mapnik ${python_site-packages}
+ln -s ${sys_python_packages}/mapnik ${env_python_packages}
 
 deactivate
