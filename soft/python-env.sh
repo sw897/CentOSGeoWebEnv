3b5ba084096a8a886e3173653e391960dc72be62
sw897 <sunwei.r@gmail.com>
2014-09-10 12:42:17 +0800

    modify pysqlite

--- soft/python-env.sh
+++ soft/python-env.sh
@@ -28,7 +28,24 @@ env_python_packages=${python_env_root}/lib/python${python_version}/site-packages
 
 source ${python_env_root}/bin/activate
 
-install_func "pysqlite"    "${env_python_packages}/pysqlite2"    "./soft/pysqlite.sh"
+# install_func "pysqlite"    "${env_python_packages}/pysqlite2"    "./soft/pysqlite.sh"
+pysqlite_version="2.6.3"
+rm -rf pysqlite-${soft_version}
+if [ ! -f pysqlite-${pysqlite_version}.tar.gz ]; then
+    wget https://pypi.python.org/packages/source/p/pysqlite/pysqlite-${pysqlite_version}.tar.gz
+fi
+tar -zxvf pysqlite-${pysqlite_version}.tar.gz
+if [ -e "./pysqlite_setup.cfg" ]; then
+    cat pysqlite_setup.cfg > pysqlite-${pysqlite_version}/setup.cfg
+elif [ -e "./soft/pysqlite_setup.cfg" ]; then
+    cat ./soft/pysqlite_setup.cfg > pysqlite-${pysqlite_version}/setup.cfg
+fi
+cd pysqlite-${pysqlite_version}
+python2.7 setup.py build
+python2.7 setup.py install
+cd ..
+rm -rf pysqlite-${pysqlite_version}
+
 pip install simplejson
 pip install lxml
 pip install pillow
