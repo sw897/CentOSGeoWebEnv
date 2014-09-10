437244f3a0fbff0bfaa2e0547dbb484a0d7f1404
sw897 <sunwei.r@gmail.com>
2014-09-10 20:26:21 +0800

    0.2

--- soft/boost.sh
+++ soft/boost.sh
@@ -10,12 +10,6 @@
 # ******************************************************************************
 #
 
-if [ -e "../inc.sh" ]; then
-    . "../inc.sh"
-elif [ -e "./inc.sh" ]; then
-    . "./inc.sh"
-fi
-
 rm -rf boost_1_56_0
 if [ ! -f boost_1_56_0.tar.gz ]; then
     wget http://jaist.dl.sourceforge.net/project/boost/boost/1.56.0/boost_1_56_0.tar.gz
@@ -23,10 +17,17 @@ fi
 tar -zxvf boost_1_56_0.tar.gz
 cd boost_1_56_0
 
-source ${python_env_root}/bin/activate
 ./bootstrap.sh
-./bjam -sTOOLS=gcc install
-deactivate
+./bjam -sTOOLS=gcc  install
+
+if [ -e "../user-config.jam" ]; then
+    cp ../user-config.jam > ./user-config.jam
+elif [ -e "../soft/user-config.jam" ]; then
+    cp ../soft/user-config.jam > ./user-config.jam
+fi
+
+bjam --with-python -a -j2 --ignore-site-config --user-config=user-config.jam toolset=gcc stage -d2
+cp stage/lib/libboost_python.so* /usr/local/lib
 
 cd ..
 rm -rf boost_1_56_0
