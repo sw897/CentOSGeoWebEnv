469ad326c732fb1ec47e52ab8f252cb93f2e22d8
sw897 <sunwei.r@gmail.com>
2014-09-11 17:16:34 +0800

    0.2

--- soft/spatialite.sh
+++ soft/spatialite.sh
@@ -12,6 +12,7 @@
 
 soft_version="4.2.0"
 
+# libspatialite
 rm -rf libspatialite-${soft_version}
 if [ ! -f libspatialite-${soft_version}.tar.gz ]; then
     wget http://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-${soft_version}.tar.gz
@@ -25,4 +26,18 @@ make install
 
 cd ..
 rm -rf libspatialite-${soft_version}
-# rm -f libspatialite-${soft_version}.tar.gz
+
+# spatialite-tools
+rm -rf spatialite-tools-${soft_version}
+if [ ! -f spatialite-tools-${soft_version}.tar.gz ]; then
+    wget http://www.gaia-gis.it/gaia-sins/spatialite-tools-sources/spatialite-tools-${soft_version}.tar.gz
+fi
+tar -zxvf spatialite-tools-${soft_version}.tar.gz
+cd spatialite-tools-${soft_version}
+
+./configure
+make
+make install
+
+cd ..
+rm -rf spatialite-tools-${soft_version}
