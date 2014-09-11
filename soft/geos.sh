47942836f889b33f5bb1aee9518f781abece612a
sw897 <sunwei.r@gmail.com>
2014-09-11 21:57:14 +0800

    0.3

--- soft/geos.sh
+++ soft/geos.sh
@@ -15,14 +15,14 @@ soft_version="3.4.2"
 rm -rf geos-${soft_version}
 if [ ! -f geos-${soft_version}.tar.bz2 ]; then
     wget http://download.osgeo.org/geos/geos-${soft_version}.tar.bz2
-    wget http://trac.osgeo.org/geos/raw-attachment/ticket/535/geos-535.patch
-    wget http://trac.osgeo.org/geos/raw-attachment/ticket/535/geos-535-makefile-in.patch
 fi
 tar -jxvf geos-${soft_version}.tar.bz2
 cd geos-${soft_version}
 
-patch -p0 < ../geos-535.patch
-patch -p0 < ../geos-535-makefile-in.patch
+wget http://trac.osgeo.org/geos/raw-attachment/ticket/535/geos-535.patch
+wget http://trac.osgeo.org/geos/raw-attachment/ticket/535/geos-535-makefile-in.patch
+patch -p0 < geos-535.patch
+patch -p0 < geos-535-makefile-in.patch
 
 ./configure
 make
@@ -30,4 +30,3 @@ make install
 
 cd ..
 rm -rf geos-${soft_version}
-# rm -f geos-${soft_version}.tar.bz2
