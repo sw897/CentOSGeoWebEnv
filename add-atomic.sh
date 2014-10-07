354d158951e962c57b8530073eaf4c1ee58a525e
sw897 <sunwei.r@gmail.com>
2014-10-07 12:03:42 +0800

    ka

---
+++ add-atomic.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# ******************************************************************************
+# Software:     CentOSGeoWebEnv
+# Author:       mapdb
+# Website:      www.mapdb.cn
+# Email:        mapdb2014@gmail.com
+# ------------------------------------++++
+# Thank you for choosing CentOSGeoWebEnv!
+# ******************************************************************************
+#
+
+wget -q -O - http://www.atomicorp.com/installers/atomic | sh
