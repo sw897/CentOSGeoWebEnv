940e7c91e4f01b5c6ce3d1718830d75dba970f4e
sw897 <sunwei.r@gmail.com>
2014-10-07 14:39:31 +0800

    add epel repo

---
+++ add-epel-repo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# ******************************************************************************
+# Software:     CentOSGeoWebEnv
+# Author:       mapdb
+# Website:      www.mapdb.cn
+# Email:        mapdb2014@gmail.com
+# ------------------------------------++++
+# Thank you for choosing CentOSGeoWebEnv!
+# ******************************************************************************
+#
+
+wget http://epel.mirror.net.in/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm -Uvh epel-release-6-8.noarch.rpm
