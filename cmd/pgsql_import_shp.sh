373feab73bf850ceba2a21d82cf29e762b1cf9ef
sw897 <sunwei.r@gmail.com>
2015-07-29 22:06:19 +0800

    u

--- cmd/pgsql_import_shp.sh
+++ cmd/pgsql_import_shp.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 db_name="postgis_wf"
-shp_file="wfshp"
 srid=4326
 su postgres -c "createdb -U postgres -E UTF-8 --template=template0 $db_name"
 su postgres -c "psql -U postgres -d $db_name -c \"CREATE EXTENSION postgis;\""
+shp_file="wfrl"
+shp2pgsql -g the_geom -s ${srid} -W GB18030 ${shp_file}.shp ${shp_file}>${shp_file}.sql
+su postgres -c "psql -U postgres -d $db_name -f ${shp_file}.sql"
+shp_file="wfrp"
 shp2pgsql -g the_geom -s ${srid} -W GB18030 ${shp_file}.shp ${shp_file}>${shp_file}.sql
 su postgres -c "psql -U postgres -d $db_name -f ${shp_file}.sql"
